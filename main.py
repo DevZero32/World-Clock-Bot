acbed6dfc387cacdd4936f309ab712696650b0a1
Dev Zero <114887200+DevZero32@users.noreply.github.com>
2022-10-16 16:53:56 +0000

    json

--- main.py
+++ main.py
@@ -1,10 +1,67 @@
 import os
 import discord
+from discord.ext import commands
+import json
+import time
 
-from discord import Intents
-
-Intents.message_content = True
-client = discord.Client(intents = Intents.all())
+client = commands.Bot(command_prefix="/",intents = discord.Intents.all())
 token = os.environ["token"]
 
-import time
+
+def load_tz():
+  with open("timezones.json", "r") as file:
+    Timezone = json.loads(file.read())
+    return Timezone
+
+def findTz(tz):
+  Timezone = load_tz()
+  for timezone in Timezone:
+    if timezone["abbr"] == tz:
+      return timezone["offset"]
+
+
+
+@client.command()
+async def stime(ctx,arg):
+  offset = int(arg)
+  epochtime = round(time.time())
+  for hour in range(offset):
+    epochtime = epochtime + 3600
+  await ctx.send(f"<t:{round(time.time())}:t>")
+
+
+
+
+@client.command()
+async def date(ctx,tz,Rtime):
+  offset = findTz(tz)
+  print(offset)
+  epochtime = round(time.time())
+  for hour in range(offset):
+    epochtime = epochtime + 3600
+  await ctx.send(f"<t:{round(time.time())}:D>")
+
+
+
+
+@client.command()
+async def week(ctx,arg):
+  offset = int(arg)
+  epochtime = round(time.time())
+  for hour in range(offset):
+    epochtime = epochtime + 3600
+  await ctx.send(f"<t:{epochtime}:F>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.run(token)
